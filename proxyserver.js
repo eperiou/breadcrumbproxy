68837fc9416265f64d3c3d221460ceda7b2c906b
Eric Periou <eperiou@yahoo.com>
2017-03-24 16:15:48 -0500

    (feature) added routes for backend communication to cloudinary and api

--- proxyserver.js
+++ proxyserver.js
@@ -2,9 +2,13 @@ const app = require('express')();
 
 const proxy = require('express-http-proxy');
 
+require('dotenv').config();
 
 // app.use('/test', proxy("http://192.168.99.100");
-// app.use('/', (req, res) => { res.send('herro'); });
-app.use('/api', proxy('http://192.168.99.100:3000/'));
-app.use('/vision', proxy('http://192.168.99.100:5000/'));
-app.listen(process.env.PORT || 7000);
+app.use('/', (req, res) => { res.send('herro'); });
+app.use('/api', proxy('localhost:3000'));//process.env.ROUTE_API
+app.use('/vision', proxy('localhost:8100/'));//process.env.ROUTE_VISION ||
+app.use('/cloudinary', proxy('localhost:5000/'));//process.env.ROUTE_CLOUDINARY ||
+app.listen(process.env.PORT || 7000
+  console.log('connected on '+ process.env.PORT)
+);
