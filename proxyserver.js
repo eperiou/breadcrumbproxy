52b819de9959917ef187b06cce322f3813c02463
Eric Periou <eperiou@yahoo.com>
2017-03-24 21:31:46 -0500

    (debug) fixed broken route to cloudinary

--- proxyserver.js
+++ proxyserver.js
@@ -1,25 +1,8 @@
-// const app = require('express')();
-//
-// const proxy = require('express-http-proxy');
-//
-// // require('dotenv').config();
-//
-// // app.use('/test', proxy("http://192.168.99.100");
-// app.use('/', (req, res) => { res.send('herro'); });
-// app.use('/api', proxy('lhttp://192.168.99.100:3000/'));//process.env.ROUTE_API
-// app.use('/vision', proxy('http://192.168.99.100:8100/'));//process.env.ROUTE_VISION ||
-// app.use('/cloudinary', proxy('http://192.168.99.100:5000/'));//process.env.ROUTE_CLOUDINARY ||
-// app.listen(process.env.PORT || 7000
-//   console.log('connected on '+ process.env.PORT)
-// );
-//
 const app = require('express')();
 
 const proxy = require('express-http-proxy');
 
-//
-// app.use('/test', proxy("http://192.168.99.100");
-// app.use('/', (req, res) => { res.send('herro'); });
+
 app.use('/api', proxy('http://192.168.99.100:3000/'));
 app.use('/submit/trail',proxy('http://192.168.99.100:5000/'));
 app.use('/vision', proxy('http://192.168.99.100:8100/'));
